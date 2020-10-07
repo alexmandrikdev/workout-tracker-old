1816c2606617d2d26581a8befb115d27a408a37d
MnDAlex <alexvok12@gmail.com>
2020-10-07 15:27:15 +0200

    Models and migrations created

---
+++ database/migrations/2020_10_07_131839_create_workout_x_exercises_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorkoutXExercisesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workout_x_exercises', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('workout_id')->constrained();
+            $table->foreignId('exercise_id')->constrained();
+            $table->unsignedTinyInteger('amount');
+            $table->enum('unit', ['s', 'rep', 'min', 'hour']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workout_x_exercises');
+    }
+}
