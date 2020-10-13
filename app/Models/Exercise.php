7abd4db5ced0dee7c5a49b4f0583d0433ebea81d
MnDAlex <alexvok12@gmail.com>
2020-10-13 16:09:22 +0200

    Exercise model: fillable set, user_id added to hidden

--- app/Models/Exercise.php
+++ app/Models/Exercise.php
@@ -10,5 +10,12 @@ class Exercise extends Model
 {
     use SoftDeletes, Multitenantable;
 
-    protected $guarded = [];
+    protected $fillable = [
+        'name',
+        'user_id',
+    ];
+
+    protected $hidden = ['created_at', 'updated_at', 'deleted_at', 'user_id'];
+
+
 }
