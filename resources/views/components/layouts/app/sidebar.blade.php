14605ca01a311d7064f4a18e8cf8a3f0ecd2db3c
MnDAlex <alexvok12@gmail.com>
2020-10-13 11:26:38 +0200

    sidebar: Calendar menu point added

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -1,6 +1,7 @@
 <div class="bg-dark text-light border-right" id="sidebar-wrapper">
     <div class="sidebar-heading">Workout Tracker</div>
     <div class="list-group list-group-flush">
+      <a href="/calendar" class="list-group-item list-group-item-action bg-dark text-light">@lang('Calendar')</a>
       <a href="/workouts" class="list-group-item list-group-item-action bg-dark text-light">@lang('My Workouts')</a>
       <a href="/workouts/import" class="list-group-item list-group-item-action bg-dark text-light">@lang('Import Workouts')</a>
       <a href="#" class="list-group-item list-group-item-action bg-dark text-light">@lang('Excersises')</a>
