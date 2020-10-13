0eaa48b22fd8063aeb52ff113deca380e3aee1ee
MnDAlex <alexvok12@gmail.com>
2020-10-13 16:14:02 +0200

    Exercise: workouts relation function added

--- app/Models/Exercise.php
+++ app/Models/Exercise.php
@@ -17,5 +17,9 @@ class Exercise extends Model
 
     protected $hidden = ['created_at', 'updated_at', 'deleted_at', 'user_id'];
 
+    public function workouts()
+    {
+        return $this->belongsToMany(Workout::class, 'workout_x_exercises')->withPivot('set', 'amount', 'unit_id');
+    }
 
 }
