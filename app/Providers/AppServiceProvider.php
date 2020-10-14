d0de5cb5818537011fe537dec3144bd112ccb797
MnDAlex <alexvok12@gmail.com>
2020-10-14 08:55:04 +0200

    Sidebar: workout names listed below My Workouts

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Workout;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $workoutNames = Workout::select('name')->get()->pluck('name')->unique();
+
+        view()->share('workoutNames', $workoutNames);
     }
 }
--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -1,9 +1,15 @@
 <div class="bg-dark text-light border-right" id="sidebar-wrapper">
     <div class="sidebar-heading">Workout Tracker</div>
     <div class="list-group list-group-flush">
-      <a href="/calendar" class="list-group-item list-group-item-action bg-dark text-light">@lang('Calendar')</a>
-      <a href="/workouts" class="list-group-item list-group-item-action bg-dark text-light">@lang('My Workouts')</a>
-      <a href="/workouts/import" class="list-group-item list-group-item-action bg-dark text-light">@lang('Import Workouts')</a>
-      <a href="#" class="list-group-item list-group-item-action bg-dark text-light">@lang('Excersises')</a>
+        <a href="/calendar" class="list-group-item list-group-item-action bg-dark text-light">@lang('Calendar')</a>
+        <a href="#workouts" data-toggle="collapse"
+            class="dropdown-toggle collapsed list-group-item list-group-item-action bg-dark text-light">@lang('My Workouts')</a>
+            <div id="workouts" class="collapse list-group">
+                @foreach ($workoutNames as $workoutName)
+                    <a href="#" class="list-group-item list-group-item-action bg-dark text-light">{{ $workoutName }}</a>
+                @endforeach
+            </div>
+        <a href="/workouts/import" class="list-group-item list-group-item-action bg-dark text-light">@lang('Import Workouts')</a>
+        <a href="#" class="list-group-item list-group-item-action bg-dark text-light">@lang('Excersises')</a>
     </div>
 </div>
