58386d7d0e68ca33bd3d67254e96f4549483b45b
MnDAlex <alexvok12@gmail.com>
2020-10-14 09:07:01 +0200

    Multitenantable trait: auth check deleted

--- app/Traits/Multitenantable.php
+++ app/Traits/Multitenantable.php
@@ -8,14 +8,12 @@ trait Multitenantable
 {
     public static function bootMultitenantable()
     {
-        if (auth()->check()) {
-            static::creating(function ($model) {
-                $model->user_id = auth()->id();
-            });
+        static::creating(function ($model) {
+            $model->user_id = auth()->id();
+        });
 
-            static::addGlobalScope('user_id', function (Builder $builder) {
-                return $builder->where('user_id', auth()->id());
-            });
-        }
+        static::addGlobalScope('user_id', function (Builder $builder) {
+            return $builder->where('user_id', auth()->id());
+        });
     }
 }
