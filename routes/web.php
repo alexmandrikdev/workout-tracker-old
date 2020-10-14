6eaa9fc3e51bc24a67e493dcf74c624e30c9a931
MnDAlex <alexvok12@gmail.com>
2020-10-14 13:34:45 +0200

    redirect from / to calendar

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'auth'], function () {
 
-    Route::redirect('/', 'workouts');
+    Route::redirect('/', 'calendar');
 
     Route::group(['prefix' => 'workouts', 'as' => 'workouts.'], function () {
         Route::group(['prefix' => 'import', 'as' => 'import.'], function () {
