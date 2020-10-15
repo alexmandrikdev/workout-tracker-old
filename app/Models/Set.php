7df7cbd69aae4a770370d0ef5796cfdf4ae2d9de
MnDAlex <alexvok12@gmail.com>
2020-10-15 13:35:13 +0200

    Set model: exercises relation added

--- app/Models/Set.php
+++ app/Models/Set.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Pivots\SetExercise;
 use App\Traits\Multitenantable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -13,4 +14,10 @@ class Set extends Model
     protected $guarded = [];
 
     protected $hidden = ['user_id', 'created_at', 'updated_at', 'deleted_at'];
+
+    public function exercises()
+    {
+        return $this->belongsToMany(Exercise::class, 'set_exercises')
+            ->withPivot(['amount', 'unit_id', 'rest_amount', 'rest_unit_id']);
+    }
 }
