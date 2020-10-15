30d24a7627599109d87e8a56b8f46430f9891b91
MnDAlex <alexvok12@gmail.com>
2020-10-15 09:10:01 +0200

    Set, SetExercise models: created_at, updated_at, deleted_at added to hidden

--- app/Models/Set.php
+++ app/Models/Set.php
@@ -12,5 +12,5 @@ class Set extends Model
 
     protected $guarded = [];
 
-    protected $hidden = ['user_id'];
+    protected $hidden = ['user_id', 'created_at', 'updated_at', 'deleted_at'];
 }
--- app/Models/SetExercise.php
+++ app/Models/SetExercise.php
@@ -12,5 +12,5 @@ class SetExercise extends Model
 
     protected $guarded = [];
 
-    protected $hidden = ['user_id'];
+    protected $hidden = ['user_id', 'created_at', 'updated_at', 'deleted_at'];
 }
