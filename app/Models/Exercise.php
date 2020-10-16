ba0dc9cad01bf6549ce880b58dd03b4c46fbd71d
MnDAlex <alexvok12@gmail.com>
2020-10-16 18:27:03 +0200

    Exercise unnecessary relation removed

--- app/Models/Exercise.php
+++ app/Models/Exercise.php
@@ -14,9 +14,4 @@ class Exercise extends Model
     protected $guarded = [];
 
     protected $hidden = ['created_at', 'updated_at', 'deleted_at', 'user_id'];
-
-    public function workouts()
-    {
-        return $this->belongsToMany(Workout::class, 'workout_x_exercises')->withPivot('set', 'amount', 'unit_id');
-    }
 }
