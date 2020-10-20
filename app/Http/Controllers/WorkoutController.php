9ce13111ae462936a5ab8b36c9aac1f01c963e66
MnDAlex <alexvok12@gmail.com>
2020-10-20 12:27:16 +0200

    WorkoutController@index removed

--- app/Http/Controllers/WorkoutController.php
+++ app/Http/Controllers/WorkoutController.php
@@ -11,55 +11,6 @@ use Illuminate\Support\Facades\Cache;
 
 class WorkoutController extends Controller
 {
-    public function index(Request $request)
-    {
-        $workoutNames = Workout::select('name')
-            ->get()->pluck('name')->unique();
-
-        $workoutDateMin = Carbon::parse(Workout::min('date'));
-        $workoutDateMax = Carbon::parse(Workout::max('date'));
-
-        $chartLabels = collect();
-
-        while ($workoutDateMin->lte($workoutDateMax)) {
-            $chartLabels->push($workoutDateMin->toDateString());
-            $workoutDateMin->addDay();
-        }
-
-        $exercises = Exercise::with([
-            'workoutExercises:workout_id,exercise_id,amount,unit_id',
-            'workoutExercises.workout:id,name,date',
-            'workoutExercises.unit:id,name',
-        ])
-            ->get()
-            ->map(function ($exercise) {
-                return [
-                    'name' => $exercise->name,
-                    'workout_name' => $exercise->workoutExercises->first()->workout->name,
-                    'workouts' => $exercise->workoutExercises->map(function ($workoutExercise) {
-                        return [
-                            'amount' => $workoutExercise->amount,
-                            'unit' => $workoutExercise->unit->name,
-                            'date' => $workoutExercise->workout->date,
-                        ];
-                    })
-                        ->groupBy('date')
-                        ->map(function ($day) {
-                            return [
-                                'amount_sum' => $day->sum('amount'),
-                                'set_count' => $day->count('amount')
-                            ];
-                        }),
-                ];
-            });
-
-        $workouts = $exercises->groupBy('workout_name');
-
-        // return $workouts;
-
-        return view('workouts.index', compact('workoutNames', 'workouts', 'chartLabels'));
-    }
-
     public function show($date)
     {
         $workout = Workout::whereDate('date', $date)->get();
