6ffc26b77c27b6a943e3dc08c06c9ea645c7f768
MnDAlex <alexvok12@gmail.com>
2020-10-20 16:25:04 +0200

    Workout show: displaying set sort fixed

--- resources/views/workouts/show.blade.php
+++ resources/views/workouts/show.blade.php
@@ -24,7 +24,12 @@
                     @foreach ($set['data']->exercises as $key => $exercise)
                         <tr class="{{ $key === $set['data']->exercises->keys()->first() ? 'exercise-first-row' : '' }}">
                             @if($key === 0)
-                                <td rowspan="{{ $set['data']->exercises->count() }}" class="align-middle text-center">{{ $set['data']->pivot->sort }}</td>
+                                <td rowspan="{{ $set['data']->exercises->count() }}" class="align-middle text-center">
+                                    <div>{{ $set['data']->pivot->sort }}</div>
+                                    @for ($i = 1; $i < $set['count']; $i++)
+                                        <div>{{ $set['data']->pivot->sort + $i }}</div>
+                                    @endfor
+                                </td>
                             @endif
                             <td class="exercise-first-col">{{ $exercise->name }}</td>
                             <td>{{ $exercise->pivot->amount }} {{ Str::plural($exercise->pivot->unit->name, $exercise->pivot->amount) }}</td>
